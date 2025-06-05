ca3157fac0796ee4df0c822f49f9cba772a3cba0
Lordwick5 <Prashantchy96@gmail.com>
2025-06-06 04:55:36 +0545

    updated code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const minNum = 0;
 const maxNum = 100;
-const answer = Math.random() * (maxNum-minNum + 1);
+const answer = Math.random() * (maxNum - minNum + 1);
 
-console.kog(answer);
+console.log(answer);
