a21da2667bb129b51669f169130b251c2827bb2f
Lordwick5 <Prashantchy96@gmail.com>
2025-06-06 04:59:20 +0545

    updated code

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const minNum = 0;
 const maxNum = 100;
 const answer = Math.random() * (maxNum - minNum + 1);
 
-console.log(answer);
+let attempts = 0;
+let guess;
+let running = true;
+
+while (running) {
+    
+    guess = window.prompt(`Guess a number between ${minNUm} - ${maxNum}`);
+    guess = Number(guess);
+    console.log(typeof guess, guess);
+    running = false;
+}
+
